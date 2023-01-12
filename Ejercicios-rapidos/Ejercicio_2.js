53c0edfccd23985b14dd4a32b0c7482aa9a444c5
LeoDavila1994 <liodavila.ld@gmail.com>
2023-01-12 13:05:18 -0600

    ejercicio-2

---
+++ Ejercicios-rapidos/Ejercicio_2.js
@@ -0,0 +1,13 @@
+const about_me = [
+  "Leonardo Dávila Martínez",
+  28,
+  true,
+  new Date(1994, 10, 28),
+  {
+  title: "Harry Potter 8",
+  autor: "Joanne Rowling",
+  date: "31 / 05 / 2016",
+  url: "https://www.amazon.com.mx/Harry-Potter-Deathly-Hallows-Grandpre/dp/0545010225/ref=sr_1_1?__mk_es_MX=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=T7BKQ3D64W9K&keywords=harry+potter+8&qid=1673550108&sprefix=harry+potter+8%2Caps%2C185&sr=8-1"
+}];
+
+console.log(about_me[about_me.length - 1]);
---
+++ sesion-01-02-mi-primer-proyecto-node/Tipos-de-datos.js
@@ -0,0 +1,30 @@
+//  Tipos primitivos:
+
+//Number:
+
+  4;
+
+// String:
+
+  "Hola";
+
+// Booleans:
+
+  true;
+  false;
+
+  // Null and Undefined:
+
+  null;
+  undefined;
+
+// Estos valores son falsy: (null, undefined, false, 0.)
+
+if(true) {
+  console.log("se cumplio");
+} else {
+  console.log("No cumple");
+}
+
+
+
