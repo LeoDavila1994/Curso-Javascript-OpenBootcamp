b71641cc67f4e9d5b192b8ef43b317b1b8c22a9a
LeoDavila1994 <liodavila.ld@gmail.com>
2023-01-15 12:35:40 -0600

    ejercicio 05

---
+++ sesion-05-numbers/index.js
@@ -0,0 +1,15 @@
+let alturaCm = 173;
+
+let alturaM = 1.73;
+
+let pesoKg = 85.3;
+
+let alturaMredondeo = Math.ceil(alturaM);
+
+let pesoKgredonde = Math.floor(pesoKg);
+
+let maxNum = Number.MAX_VALUE;
+
+let maxNumSuma = maxNum + 1;
+
+console.log(maxNumSuma === maxNum);
