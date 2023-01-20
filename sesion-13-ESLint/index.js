08736413ac7bb1d887d58bf19bfae05204fc21cb
LeoDavila1994 <liodavila.ld@gmail.com>
2023-01-20 14:18:38 -0600

    ejercicio 13 donde

--- sesion-13-ESLint/index.js
+++ sesion-13-ESLint/index.js
@@ -1,2 +1,22 @@
+let username = "Leonardo";
 
+let lastname = "Dávila Martínez";
+
+let student = username.concat(" ").concat(lastname);
+
+let studentMayus = student.toUpperCase();
+
+let studentMinus = student.toLowerCase();
+
+let numOfCharacters = student.length;
+
+let firstLetter = username.charAt(0);
+
+let lastLetter = lastname.charAt(lastname.length - 1);
+
+let withoutWhiteSpaces = student.replace(/ /g, "");
+
+let isIncluding = student.includes(username);
+
+console.log(student);
 
