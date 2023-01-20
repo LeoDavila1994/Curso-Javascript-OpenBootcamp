5b33e3c25fd07566afdbed0cd721f72d02c2a725
LeoDavila1994 <liodavila.ld@gmail.com>
2023-01-19 19:55:18 -0600

    ejercicio 12

---
+++ sesion-12-debuger/index.js
@@ -0,0 +1,19 @@
+let num = 6;
+let arr = [0,1];
+let x = 1;
+
+const fibonacci = (limit) => {
+
+  x++;
+  arr.push(arr[arr.length -2] + arr[arr.length - 1]);
+
+  if (x === limit){
+    return arr;
+  }
+
+  fibonacci(num);
+};
+
+fibonacci();
+
+console.log(arr);
